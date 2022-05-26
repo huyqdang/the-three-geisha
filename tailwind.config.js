ee56b701694cf4f636402cd5749f3bed76489e67
Ellen Kwan <eh2.kwan@gmail.com>
2022-05-26 07:44:19 -0700

    install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'media', // 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
