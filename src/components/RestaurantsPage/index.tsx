b0ee12c9c4ab7cdf5066e84cdaa0a380f421b629
Ellen Kwan <eh2.kwan@gmail.com>
2022-05-27 03:06:03 -0700

    wip

--- src/components/RestaurantsPage/index.tsx
+++ src/components/RestaurantsPage/index.tsx
@@ -1,17 +1,41 @@
-import { Component, createResource, createSignal, onMount } from 'solid-js'
+import { Component, createEffect, createSignal, Index, onMount } from 'solid-js'
 import { mapLoader, defaultOption } from '../../utils/map'
+import mockBrandData from '../../../mock_server/brand_mapping.json'
 import './restaurantsPage.css'
 
-const fetchReviews = async (appId: string) => {
-    (await fetch(`http://localhost:8080/reviews?app_id=${appId}`)).json()
+const filterURL = `http://localhost:8080/locations/nearby`
+const radius = 10 // in miles
+
+const generateLogos = (imgSrcs: any) => {
+    const logos = imgSrcs.reduce((acc: any, imgSrc: any) => {
+        return (
+            acc +
+            `<a href="#"><img src="${imgSrc}" onclick="console.log('clicked')"/></a>`
+        )
+    }, '')
+    return '<div class="marker-popup-wrapper">' + logos + '</div>'
+}
+
+const generateStars = (qty: number) => {
+    let stars = '⭐'
+    for (let i = 0; i < qty - 1; i++) {
+        stars = stars + '⭐'
+    }
+    return stars
+}
+
+const formatName = (authorName: string) => {
+    const index = authorName.split('').indexOf(' ')
+    return `~ ${authorName.slice(0, index)} ${authorName.slice(
+        index + 1,
+        index + 2
+    )}.`
 }
 
 export const RestaurantPage: Component = () => {
-    const filterURL = `http://localhost:8080/locations/nearby`
-    const radius = 10 // in miles
-
-    const [appId] = createSignal('O9t735t013S')
-    const [reviews] = createResource(appId, fetchReviews)
+    const [reviews, setReviews] = createSignal([])
+    const [rating, setRating] = createSignal()
+    const [name, setName] = createSignal()
 
     onMount(() => {
         mapLoader.load().then((google) => {
@@ -55,25 +79,107 @@ export const RestaurantPage: Component = () => {
                         .then((res) => {
                             return res.json()
                         })
-                        .then((data) => {
-                            console.log(data)
+                        .then((locations) => {
+                            const dupMarkers = locations.reduce(
+                                (acc: any, item: any) => {
+                                    const longlatId = `${item.latitude}-${item.longitude}`
+                                    if (acc[longlatId]) {
+                                        acc[longlatId] = [
+                                            ...acc[longlatId],
+                                            item,
+                                        ]
+                                    } else {
+                                        acc[longlatId] = [item]
+                                    }
+                                    return acc
+                                },
+                                {}
+                            )
+
+                            Object.keys(dupMarkers).map((key: any) => {
+                                const locMarker = new google.maps.Marker({
+                                    icon: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
+                                    map: map,
+                                    position: {
+                                        lat: dupMarkers[key][0].latitude,
+                                        lng: dupMarkers[key][0].longitude,
+                                    },
+                                })
+                                const imgSrcs = dupMarkers[key].map(
+                                    (marker: any) => {
+                                        return mockBrandData[marker.brand_id]
+                                            .logo_url
+                                    }
+                                )
+
+                                const infoWindow = new google.maps.InfoWindow({
+                                    content: generateLogos(imgSrcs),
+                                })
+
+                                infoWindow.open({
+                                    anchor: locMarker,
+                                    map,
+                                    shouldFocus: false,
+                                })
+                                infoWindow.addListener('closeClick', () => {
+                                    infoWindow.close()
+                                })
+                            })
                         })
                 }
             })
         })
     })
 
-    console.log('reviews', reviews());
+    createEffect(async () => {
+        const temp = []
+        try {
+            const data = await fetch(
+                `http://localhost:8080/reviews?app_id=O9t735t013S`
+            )
+            if (!data.ok) {
+                console.log('Shit aint working', data.status)
+            }
+            const responseJson = await data.json()
+            const placeDetails = await fetch(
+                `http://localhost:8080/getDetails?place_id=${responseJson[0].result.place_id}`
+            )
+            const place = await placeDetails.json()
+            console.log('placeDetails', place)
+            setReviews(place.result.reviews)
+            setRating(place.result.rating)
+            setName(place.result.name)
+        } catch (err) {
+            console.log('Error here...', err)
+        }
+    }, [reviews])
 
     return (
         <section class="mt-7">
             <input class="search-bar" placeholder="Search by address here" />
-            <div class="map-container">
+            <div class="map-container mt-8">
                 <div class="g-map" id="map"></div>
-                <div class="h-full w-2/6 flex-none border-solid border-grey-200 border-2 rounded-xl ml-4 p-4">
-                    <a class="font-bold text-lg">More Info</a>
-                    <pre>{JSON.stringify(reviews(), null, 2)}</pre>
-                </div>
+                {reviews().length > 0 && (
+                    <div class="h-full w-2/6 flex-none border-solid border-grey-200 border-2 rounded-xl ml-4 p-5">
+                        <div class="font-bold text-xl w-full">{`${name()}`}</div>
+                        <div class="font-bold text-xl w-full text-gray-400">{`${rating()} Stars`}</div>
+                        <Index each={reviews()}>
+                            {(review, i) => (
+                                <div class="pb-4 pt-4">
+                                    <div class="font-bold">{`${generateStars(
+                                        review().rating
+                                    )} • ${
+                                        review().relative_time_description
+                                    }`}</div>
+                                    <div class="text-gray-400 font-semibold pt-1">
+                                        {formatName(review().author_name)}
+                                    </div>
+                                    <div class="italic">{review().text}</div>
+                                </div>
+                            )}
+                        </Index>
+                    </div>
+                )}
             </div>
         </section>
     )
