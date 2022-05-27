1501cef64a34886604990005157a3cdf764b414c
Huy Dang <hqdang97@gmail.com>
2022-05-26 21:37:52 -0600

    add popups

--- src/components/RestaurantsPage/index.tsx
+++ src/components/RestaurantsPage/index.tsx
@@ -1,10 +1,18 @@
 import { Component, onMount } from 'solid-js'
 import { mapLoader, defaultOption } from '../../utils/map'
+import mockBrandData from '../../../mock_server/brand_mapping.json'
 import './restaurantsPage.css'
 
+const filterURL = `http://localhost:8080/locations/nearby`
+const radius = 10 // in miles
+
+const generateLogos = (imgSrcs: any) => {
+    const logos = imgSrcs.reduce((acc: any, imgSrc: any) => {
+        return acc + `<img src="${imgSrc}" />`
+    }, '')
+    return '<div class="marker-popup-wrapper">' + logos + '</div>'
+}
 export const RestaurantPage: Component = () => {
-    const filterURL = `http://localhost:8080/locations/nearby`
-    const radius = 10; // in miles
     onMount(() => {
         mapLoader.load().then((google) => {
             const map = new google.maps.Map(
@@ -41,11 +49,59 @@ export const RestaurantPage: Component = () => {
                     map.setZoom(13)
                     marker.setPosition(place.geometry.location)
                     marker.setVisible(true)
-                    fetch(`${filterURL}?lat=${place.geometry.location.lat()}&lng=${place.geometry.location.lng()}&radius=${radius}`)
-                    .then(res => {return res.json()})
-                    .then(data => {
-                        console.log(data);
-                    })
+                    fetch(
+                        `${filterURL}?lat=${place.geometry.location.lat()}&lng=${place.geometry.location.lng()}&radius=${radius}`
+                    )
+                        .then((res) => {
+                            return res.json()
+                        })
+                        .then((locations) => {
+                            const dupMarkers = locations.reduce(
+                                (acc: any, item: any) => {
+                                    const longlatId = `${item.latitude}-${item.longitude}`
+                                    if (acc[longlatId]) {
+                                        acc[longlatId] = [
+                                            ...acc[longlatId],
+                                            item,
+                                        ]
+                                    } else {
+                                        acc[longlatId] = [item]
+                                    }
+                                    return acc
+                                },
+                                {}
+                            )
+
+                            Object.keys(dupMarkers).map((key: any) => {
+                                const locMarker = new google.maps.Marker({
+                                    icon: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-violet.png',
+                                    map: map,
+                                    position: {
+                                        lat: dupMarkers[key][0].latitude,
+                                        lng: dupMarkers[key][0].longitude,
+                                    },
+                                })
+                                const imgSrcs = dupMarkers[key].map(
+                                    (marker: any) => {
+                                        return mockBrandData[marker.brand_id]
+                                            .logo_url
+                                    }
+                                )
+
+                                const infoWindow = new google.maps.InfoWindow({
+                                    content: generateLogos(imgSrcs),
+                                })
+
+                                infoWindow.open({
+                                    anchor: locMarker,
+                                    map,
+                                    shouldFocus: false,
+                                })
+                                infoWindow.addListener('closeClick', () => {
+                                    infoWindow.close()
+                                })
+                            })
+                        })
                 }
             })
         })
